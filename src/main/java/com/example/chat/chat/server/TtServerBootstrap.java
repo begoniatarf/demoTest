29124a14c75c47bad6c85136e51e49fb8790791e
haoc <haoc@rihuisoft.com>
2020-07-21 18:11:00 +0800

    netty

--- src/main/java/com/example/chat/chat/server/TtServerBootstrap.java
+++ src/main/java/com/example/chat/chat/server/TtServerBootstrap.java
@@ -1,10 +1,5 @@
 package com.example.chat.chat.server;
 
-import com.example.chat.chat.config.NioWebSocketChannelInitializer;
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,25 +7,6 @@ import org.springframework.stereotype.Component;
 public class TtServerBootstrap implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-
-        NioEventLoopGroup boss=new NioEventLoopGroup();
-        NioEventLoopGroup work=new NioEventLoopGroup();
-        try {
-            ServerBootstrap bootstrap=new ServerBootstrap();
-            bootstrap.group(boss,work);
-            bootstrap.channel(NioServerSocketChannel.class);
-            bootstrap.childHandler(new NioWebSocketChannelInitializer());
-            Channel channel = bootstrap.bind(9999).sync().channel();
-            System.out.println("webSocket服务器启动成功："+channel);
-            channel.closeFuture().sync();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            System.out.println("运行出错："+e);
-        }finally {
-            boss.shutdownGracefully();
-            work.shutdownGracefully();
-            System.out.println("websocket服务器已关闭");
-        }
-
+        new SimpleChatServer(9999).run();
     }
 }
